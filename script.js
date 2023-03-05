f6b4a6900b49f45ed452c0f7cb1bc6c348092da6
Olaniyan <olaniyaneniola0225@gmail.com>
2023-03-05 21:59:46 +0100

    added

---
+++ script.js
@@ -0,0 +1,35 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const dotsNav = document.querySelector('.dots-nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+};
+
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('span');
+    if (!targetDot) return;
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+});
